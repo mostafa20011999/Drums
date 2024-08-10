bde8c5f4168c48a5eb9f1510f119a0dfa850e36f
mostafa20011999 <sasamostafa300@gmail.com>
2024-08-10 21:17:16 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,92 @@
+var numOfButtons = document.querySelectorAll("button").length;
+for (var i = 0 ; i < numOfButtons ; i++)
+{
+    document.querySelectorAll("button")[i].addEventListener("click",clickBtn);
+
+}
+function clickBtn(){
+var inner = this.innerHTML;
+flashing(this.innerHTML);
+switch (inner) {
+    case "w":
+        var crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+        case "a":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+            case "s":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+                case "d":
+                    var tom1 = new Audio("./sounds/tom-1.mp3");
+                    tom1.play();
+                    break;
+                    case "j":
+                        var tom2 = new Audio("./sounds/tom-2.mp3");
+                        tom2.play();
+                        break;
+                        case "k":
+                            var tom3 = new Audio("./sounds/tom-3.mp3");
+                            tom3.play();
+                            break;
+                            case "l":
+                                var tom4 = new Audio("./sounds/tom-4.mp3");
+                                tom4.play();
+                                break;
+
+    default:
+        console.log("not me");
+        break;
+}
+}
+document.addEventListener("keydown",keyboard);
+function keyboard(keyStroke)
+{
+  var key = keyStroke.key;
+ 
+switch(key){
+    case "w":
+        var crash = new Audio("./sounds/crash.mp3");
+        crash.play();
+        break;
+        case "a":
+            var kick = new Audio("./sounds/kick-bass.mp3");
+            kick.play();
+            break;
+            case "s":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+                case "d":
+                    var tom1 = new Audio("./sounds/tom-1.mp3");
+                    tom1.play();
+                    break;
+                    case "j":
+                        var tom2 = new Audio("./sounds/tom-2.mp3");
+                        tom2.play();
+                        break;
+                        case "k":
+                            var tom3 = new Audio("./sounds/tom-3.mp3");
+                            tom3.play();
+                            break;
+                            case "l":
+                                var tom4 = new Audio("./sounds/tom-4.mp3");
+                                tom4.play();
+                                break;
+
+    default:
+        console.log("not me");
+        break;
+}
+flashing(key);
+
+}
+function flashing(key)
+{
+var object = document.querySelector("."+key);
+object.classList.add("pressed");
+setTimeout(function(){object.classList.remove("pressed")},300);
+}
